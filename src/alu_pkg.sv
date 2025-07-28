e3e34e7c2a71a5014fc77ccbe555ab78a9fd03aa
ShubhamP1520 <shubhampotadar3@gmail.com>
2025-07-28 22:23:14 +0530

    Create alu_pkg.sv

---
+++ src/alu_pkg.sv
@@ -0,0 +1,12 @@
+package alu_pkg;
+  //`include "alu.v"
+  `include "alu_transaction.sv"
+  `include "alu_generator.sv"
+  `include "driver.sv"
+  `include "alu_reference_model.sv"
+  `include "alu_monitor.sv"
+  `include "alu_scoreboard.sv"
+  `include "alu_environment.sv"
+  `include "alu_test.sv"
+  //`include "alu.v"
+endpackage
